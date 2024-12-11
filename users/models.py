75ae69aa4c36689f604e3c88f1400b7c62a490fc
Maharshi Shastri <141903007+MaharshiShastri@users.noreply.github.com>
2024-12-11 10:48:52 +0530

    Add files via upload

---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    groups = models.ManyToManyField(
+        Group,
+        related_name='customuser_set',
+        blank=True,
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='customuser_set',
+        blank=True,
+    )
