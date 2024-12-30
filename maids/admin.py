8e3d29bcac4c8f718c83d73cc3a0a99337ff67c1
Maharshi Shastri <141903007+MaharshiShastri@users.noreply.github.com>
2024-12-30 16:36:24 +0530

    Delete maids directory

--- maids/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from customer.models import CustomUser as cust
-from maids.models import CustomUser
-# Register your models here.
-admin.site.register(CustomUser)
-admin.site.register(cust)
